1b14730883c8bcdf153104e4eb1e9601947936af
hugh19se <hugh19s@icloud.com>
2024-10-11 19:05:39 -0700

    Added Station class

---
+++ WinFormsRadio/WinFormsRadio.Forms/Station.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WinFormsRadio.Forms
+{
+    public class Station
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("icon")]
+        public string Icon { get; set; }
+        [JsonPropertyName("url")]
+        public string URL { get; set; }
+    }
+}
